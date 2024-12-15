c1eab40e97e72f573ce2281a1d345dcecfade16c
Arman Mokammel <armanmokammel@gmail.com>
2024-12-15 20:52:10 +0600

    Started working on models

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from core.models import Token_Session
 
 # Register your models here.
+@admin.register(Token_Session)
+class SessionAdmin(admin.ModelAdmin):
+    
+    list_display = ['session_name', 'is_independent', 'is_active']
+
+    def save_model(self, request, obj, form, change):
+        print(form.data)
+        if(form.data.get('is_active') == 'on' and (form.data.get('is_independent') == 'off' or obj.is_independent == False)):
+                Token_Session.objects.filter(is_active=True, is_independent=False).update(is_active=False)
+
+        super().save_model(request, obj, form, change)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
 # Create your models here.
+class Registered_User(models.Model):
+    university_id=models.CharField(null=False, blank=False, max_length=20)
+    name=models.CharField(null=False, blank=False, max_length=150)
+    university=models.CharField(null=False, blank=False, max_length=150)
+
+    class Meta:
+        verbose_name="Registered User"
+
+    def __self__(self) -> str:
+        return str(self.pk)
+
+class Token_Session(models.Model):
+    session_name=models.CharField(null=False, blank=False, max_length=20)
+    is_active=models.BooleanField(null=False, blank=False, default=False)
+    is_independent=models.BooleanField(null=False, blank=False, default=False)
+
+    class Meta:
+        verbose_name="Token Session"
+
+    def current_session():
+        return Token_Session.objects.filter(is_active=True)
+
+    def save(self, *args, **kwargs):
+        if (self.pk):
+            if(kwargs.get('is_active') == 'on' and (kwargs.get('is_independent') == 'off' or self.is_independent == False)):
+                Token_Session.objects.filter(is_active=True, is_independent=False).update(is_active=False)
+        
+        super().save(*args, **kwargs)
+
+
+    def __self__(self):
+        return self.pk
